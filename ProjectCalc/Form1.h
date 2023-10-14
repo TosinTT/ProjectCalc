6cabfddfa74d489ae4076f7cf1f892c0615de817
TosinT <talabitosin67@gmail.com>
2023-10-14 08:09:09 +0100

    Add project files.

---
+++ ProjectCalc/Form1.h
@@ -0,0 +1,493 @@
+﻿#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ txtDisplay;
+	protected:
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ btnErase;
+
+	private: System::Windows::Forms::Button^ btnClear;
+
+	private: System::Windows::Forms::Button^ btnCE;
+
+	private: System::Windows::Forms::Button^ btnAddSub;
+
+	private: System::Windows::Forms::Button^ btn7;
+	private: System::Windows::Forms::Button^ btn8;
+	private: System::Windows::Forms::Button^ btn9;
+	private: System::Windows::Forms::Button^ btnAdd;
+
+
+
+
+	private: System::Windows::Forms::Button^ btn4;
+
+	private: System::Windows::Forms::Button^ btn5;
+	private: System::Windows::Forms::Button^ btn6;
+	private: System::Windows::Forms::Button^ btnSub;
+
+
+
+
+	private: System::Windows::Forms::Button^ btn3;
+
+	private: System::Windows::Forms::Button^ btn2;
+
+	private: System::Windows::Forms::Button^ btn1;
+	private: System::Windows::Forms::Button^ btnDiv;
+
+
+	private: System::Windows::Forms::Button^ btn0;
+	private: System::Windows::Forms::Button^ btnPoint;
+
+
+
+	private: System::Windows::Forms::Button^ btnEqual;
+
+	private: System::Windows::Forms::Button^ btnMultiply;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->txtDisplay = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->btnErase = (gcnew System::Windows::Forms::Button());
+			this->btnClear = (gcnew System::Windows::Forms::Button());
+			this->btnCE = (gcnew System::Windows::Forms::Button());
+			this->btnAddSub = (gcnew System::Windows::Forms::Button());
+			this->btn7 = (gcnew System::Windows::Forms::Button());
+			this->btn8 = (gcnew System::Windows::Forms::Button());
+			this->btn9 = (gcnew System::Windows::Forms::Button());
+			this->btnAdd = (gcnew System::Windows::Forms::Button());
+			this->btn4 = (gcnew System::Windows::Forms::Button());
+			this->btn5 = (gcnew System::Windows::Forms::Button());
+			this->btn6 = (gcnew System::Windows::Forms::Button());
+			this->btnSub = (gcnew System::Windows::Forms::Button());
+			this->btn3 = (gcnew System::Windows::Forms::Button());
+			this->btn2 = (gcnew System::Windows::Forms::Button());
+			this->btn1 = (gcnew System::Windows::Forms::Button());
+			this->btnDiv = (gcnew System::Windows::Forms::Button());
+			this->btn0 = (gcnew System::Windows::Forms::Button());
+			this->btnPoint = (gcnew System::Windows::Forms::Button());
+			this->btnEqual = (gcnew System::Windows::Forms::Button());
+			this->btnMultiply = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// txtDisplay
+			// 
+			this->txtDisplay->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->txtDisplay->Location = System::Drawing::Point(12, 12);
+			this->txtDisplay->Multiline = true;
+			this->txtDisplay->Name = L"txtDisplay";
+			this->txtDisplay->Size = System::Drawing::Size(329, 53);
+			this->txtDisplay->TabIndex = 0;
+			this->txtDisplay->Text = L"0";
+			this->txtDisplay->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			this->txtDisplay->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(344, 150);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// btnErase
+			// 
+			this->btnErase->Font = (gcnew System::Drawing::Font(L"Wingdings", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(2)));
+			this->btnErase->Location = System::Drawing::Point(12, 81);
+			this->btnErase->Name = L"btnErase";
+			this->btnErase->Size = System::Drawing::Size(75, 66);
+			this->btnErase->TabIndex = 2;
+			this->btnErase->Text = L"";
+			this->btnErase->UseVisualStyleBackColor = true;
+			this->btnErase->Click += gcnew System::EventHandler(this, &Form1::btnErase_Click);
+			// 
+			// btnClear
+			// 
+			this->btnClear->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnClear->Location = System::Drawing::Point(93, 81);
+			this->btnClear->Name = L"btnClear";
+			this->btnClear->Size = System::Drawing::Size(75, 66);
+			this->btnClear->TabIndex = 2;
+			this->btnClear->Text = L"C";
+			this->btnClear->UseVisualStyleBackColor = true;
+			this->btnClear->Click += gcnew System::EventHandler(this, &Form1::btnClear_Click);
+			// 
+			// btnCE
+			// 
+			this->btnCE->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnCE->Location = System::Drawing::Point(174, 81);
+			this->btnCE->Name = L"btnCE";
+			this->btnCE->Size = System::Drawing::Size(75, 66);
+			this->btnCE->TabIndex = 2;
+			this->btnCE->Text = L"CE";
+			this->btnCE->UseVisualStyleBackColor = true;
+			this->btnCE->Click += gcnew System::EventHandler(this, &Form1::btnCE_Click);
+			// 
+			// btnAddSub
+			// 
+			this->btnAddSub->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAddSub->Location = System::Drawing::Point(255, 81);
+			this->btnAddSub->Name = L"btnAddSub";
+			this->btnAddSub->Size = System::Drawing::Size(75, 66);
+			this->btnAddSub->TabIndex = 2;
+			this->btnAddSub->Text = L"±";
+			this->btnAddSub->UseVisualStyleBackColor = true;
+			this->btnAddSub->Click += gcnew System::EventHandler(this, &Form1::btnAddSub_Click);
+			// 
+			// btn7
+			// 
+			this->btn7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn7->Location = System::Drawing::Point(12, 153);
+			this->btn7->Name = L"btn7";
+			this->btn7->Size = System::Drawing::Size(75, 66);
+			this->btn7->TabIndex = 2;
+			this->btn7->Text = L"7";
+			this->btn7->UseVisualStyleBackColor = true;
+			this->btn7->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn8
+			// 
+			this->btn8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn8->Location = System::Drawing::Point(93, 153);
+			this->btn8->Name = L"btn8";
+			this->btn8->Size = System::Drawing::Size(75, 66);
+			this->btn8->TabIndex = 2;
+			this->btn8->Text = L"8";
+			this->btn8->UseVisualStyleBackColor = true;
+			this->btn8->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn9
+			// 
+			this->btn9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn9->Location = System::Drawing::Point(174, 153);
+			this->btn9->Name = L"btn9";
+			this->btn9->Size = System::Drawing::Size(75, 66);
+			this->btn9->TabIndex = 2;
+			this->btn9->Text = L"9";
+			this->btn9->UseVisualStyleBackColor = true;
+			this->btn9->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btnAdd
+			// 
+			this->btnAdd->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAdd->Location = System::Drawing::Point(255, 150);
+			this->btnAdd->Name = L"btnAdd";
+			this->btnAdd->Size = System::Drawing::Size(75, 66);
+			this->btnAdd->TabIndex = 2;
+			this->btnAdd->Text = L"+";
+			this->btnAdd->UseVisualStyleBackColor = true;
+			this->btnAdd->Click += gcnew System::EventHandler(this, &Form1::ArithmeticOP);
+			// 
+			// btn4
+			// 
+			this->btn4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn4->Location = System::Drawing::Point(12, 225);
+			this->btn4->Name = L"btn4";
+			this->btn4->Size = System::Drawing::Size(75, 66);
+			this->btn4->TabIndex = 2;
+			this->btn4->Text = L"4";
+			this->btn4->UseVisualStyleBackColor = true;
+			this->btn4->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn5
+			// 
+			this->btn5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn5->Location = System::Drawing::Point(93, 225);
+			this->btn5->Name = L"btn5";
+			this->btn5->Size = System::Drawing::Size(75, 66);
+			this->btn5->TabIndex = 2;
+			this->btn5->Text = L"5";
+			this->btn5->UseVisualStyleBackColor = true;
+			this->btn5->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn6
+			// 
+			this->btn6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn6->Location = System::Drawing::Point(174, 225);
+			this->btn6->Name = L"btn6";
+			this->btn6->Size = System::Drawing::Size(75, 66);
+			this->btn6->TabIndex = 2;
+			this->btn6->Text = L"6";
+			this->btn6->UseVisualStyleBackColor = true;
+			this->btn6->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btnSub
+			// 
+			this->btnSub->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnSub->Location = System::Drawing::Point(255, 222);
+			this->btnSub->Name = L"btnSub";
+			this->btnSub->Size = System::Drawing::Size(75, 66);
+			this->btnSub->TabIndex = 2;
+			this->btnSub->Text = L"-";
+			this->btnSub->UseVisualStyleBackColor = true;
+			this->btnSub->Click += gcnew System::EventHandler(this, &Form1::ArithmeticOP);
+			// 
+			// btn3
+			// 
+			this->btn3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn3->Location = System::Drawing::Point(12, 297);
+			this->btn3->Name = L"btn3";
+			this->btn3->Size = System::Drawing::Size(75, 66);
+			this->btn3->TabIndex = 2;
+			this->btn3->Text = L"3";
+			this->btn3->UseVisualStyleBackColor = true;
+			this->btn3->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn2
+			// 
+			this->btn2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn2->Location = System::Drawing::Point(93, 297);
+			this->btn2->Name = L"btn2";
+			this->btn2->Size = System::Drawing::Size(75, 66);
+			this->btn2->TabIndex = 2;
+			this->btn2->Text = L"2";
+			this->btn2->UseVisualStyleBackColor = true;
+			this->btn2->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btn1
+			// 
+			this->btn1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn1->Location = System::Drawing::Point(174, 297);
+			this->btn1->Name = L"btn1";
+			this->btn1->Size = System::Drawing::Size(75, 66);
+			this->btn1->TabIndex = 2;
+			this->btn1->Text = L"1";
+			this->btn1->UseVisualStyleBackColor = true;
+			this->btn1->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btnDiv
+			// 
+			this->btnDiv->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnDiv->Location = System::Drawing::Point(255, 294);
+			this->btnDiv->Name = L"btnDiv";
+			this->btnDiv->Size = System::Drawing::Size(75, 66);
+			this->btnDiv->TabIndex = 2;
+			this->btnDiv->Text = L"/";
+			this->btnDiv->UseVisualStyleBackColor = true;
+			this->btnDiv->Click += gcnew System::EventHandler(this, &Form1::ArithmeticOP);
+			// 
+			// btn0
+			// 
+			this->btn0->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn0->Location = System::Drawing::Point(12, 369);
+			this->btn0->Name = L"btn0";
+			this->btn0->Size = System::Drawing::Size(75, 66);
+			this->btn0->TabIndex = 2;
+			this->btn0->Text = L"0";
+			this->btn0->UseVisualStyleBackColor = true;
+			this->btn0->Click += gcnew System::EventHandler(this, &Form1::NumbersOnly);
+			// 
+			// btnPoint
+			// 
+			this->btnPoint->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnPoint->Location = System::Drawing::Point(93, 369);
+			this->btnPoint->Name = L"btnPoint";
+			this->btnPoint->Size = System::Drawing::Size(75, 66);
+			this->btnPoint->TabIndex = 2;
+			this->btnPoint->Text = L".";
+			this->btnPoint->UseVisualStyleBackColor = true;
+			this->btnPoint->Click += gcnew System::EventHandler(this, &Form1::btnPoint_Click);
+			// 
+			// btnEqual
+			// 
+			this->btnEqual->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnEqual->Location = System::Drawing::Point(174, 369);
+			this->btnEqual->Name = L"btnEqual";
+			this->btnEqual->Size = System::Drawing::Size(75, 66);
+			this->btnEqual->TabIndex = 2;
+			this->btnEqual->Text = L"=";
+			this->btnEqual->UseVisualStyleBackColor = true;
+			this->btnEqual->Click += gcnew System::EventHandler(this, &Form1::btnEqual_Click);
+			// 
+			// btnMultiply
+			// 
+			this->btnMultiply->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnMultiply->Location = System::Drawing::Point(255, 366);
+			this->btnMultiply->Name = L"btnMultiply";
+			this->btnMultiply->Size = System::Drawing::Size(75, 66);
+			this->btnMultiply->TabIndex = 2;
+			this->btnMultiply->Text = L"*";
+			this->btnMultiply->UseVisualStyleBackColor = true;
+			this->btnMultiply->Click += gcnew System::EventHandler(this, &Form1::ArithmeticOP);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(353, 452);
+			this->Controls->Add(this->btnMultiply);
+			this->Controls->Add(this->btnDiv);
+			this->Controls->Add(this->btnSub);
+			this->Controls->Add(this->btnAdd);
+			this->Controls->Add(this->btnAddSub);
+			this->Controls->Add(this->btnEqual);
+			this->Controls->Add(this->btn1);
+			this->Controls->Add(this->btnPoint);
+			this->Controls->Add(this->btn6);
+			this->Controls->Add(this->btn2);
+			this->Controls->Add(this->btn9);
+			this->Controls->Add(this->btn5);
+			this->Controls->Add(this->btn0);
+			this->Controls->Add(this->btnCE);
+			this->Controls->Add(this->btn3);
+			this->Controls->Add(this->btn8);
+			this->Controls->Add(this->btn4);
+			this->Controls->Add(this->btnClear);
+			this->Controls->Add(this->btn7);
+			this->Controls->Add(this->btnErase);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->txtDisplay);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (txtDisplay->Text == "") {
+			txtDisplay->Text = "0";
+		}
+
+	}
+		   
+		   double firstNum, secondNum, answer;
+		   String^ charOP;
+private: System::Void NumbersOnly(System::Object^ sender, System::EventArgs^ e) {
+	Button^ numb = safe_cast<Button^>(sender);
+
+	if (txtDisplay->Text == "0") {
+		txtDisplay->Text = numb->Text;	
+	}
+	else {
+		txtDisplay->Text = txtDisplay->Text + numb->Text;
+	}
+}
+private: System::Void ArithmeticOP(System::Object^ sender, System::EventArgs^ e) {
+	Button^ Aop = safe_cast<Button^>(sender);
+	firstNum = double::Parse(txtDisplay->Text);
+	txtDisplay->Text = "";
+	charOP = Aop->Text;
+}
+private: System::Void btnClear_Click(System::Object^ sender, System::EventArgs^ e) {
+	txtDisplay->Text = "0";
+}
+private: System::Void btnCE_Click(System::Object^ sender, System::EventArgs^ e) {
+	txtDisplay->Text = "0";
+}
+private: System::Void btnPoint_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (!txtDisplay->Text->Contains(".")) {
+		txtDisplay->Text = txtDisplay->Text + ".";
+	}
+}
+private: System::Void btnErase_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (txtDisplay->Text->Length>0) {
+		txtDisplay->Text = txtDisplay->Text->Remove(txtDisplay->Text->Length -1,1);
+	}
+}
+private: System::Void btnAddSub_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	if (txtDisplay->Text->Contains("-")) {
+		txtDisplay->Text = txtDisplay->Text->Remove(0, 1);
+	}
+	else {
+		txtDisplay->Text = "-" + txtDisplay->Text;
+	}
+}
+private: System::Void btnEqual_Click(System::Object^ sender, System::EventArgs^ e) {
+	secondNum = double::Parse(txtDisplay->Text);
+	if (charOP == "+") {
+		answer = firstNum + secondNum;
+		txtDisplay->Text = System::Convert::ToString(answer);
+	}
+	else if (charOP == "-") {
+		answer = firstNum - secondNum;
+		txtDisplay->Text = System::Convert::ToString(answer);
+	}
+	else if (charOP == "*") {
+		answer = firstNum *secondNum;
+		txtDisplay->Text = System::Convert::ToString(answer);
+	}
+	else if (charOP == "/") {
+		answer = firstNum / secondNum;
+		txtDisplay->Text = System::Convert::ToString(answer);
+	}
+
+}
+};
+}
+ 
